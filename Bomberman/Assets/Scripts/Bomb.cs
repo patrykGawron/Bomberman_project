18329cba73561ce727a7542adae1f4918c683ed3
Patryk Gawron <39757782+patrykGawron@users.noreply.github.com>
2020-06-01 19:37:32 +0200

    Added bomb animations and interactions
    Bombs are now animated and can destroy walls. Destruction beam is scalable.

---
+++ Bomberman/Assets/Scripts/Bomb.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float lifetime = 4.3f;
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+
+        if(lifetime <= 0 )
+        {
+            FindObjectOfType<WallDestroyer>().Explode(transform.position);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Bomberman/Assets/Scripts/WallDestroyer.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+
+public class WallDestroyer : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+    public Tile unbreakableWall;
+    public Tile wall;
+    //public int explosionLength;
+
+    public GameObject middleExplosion;
+    public GameObject verticalExplosion;
+    public GameObject horizontalExplosion;
+    public GameObject topEndExplosion;
+    public GameObject bottomEndExplosion;
+    public GameObject rightEndExplosion;
+    public GameObject leftEndExplosion;
+
+    public int explosionLength = 1;
+    
+
+
+    public void Explode(Vector2 worldPos)
+    {
+        Vector3Int originCell = tilemap.WorldToCell(worldPos);
+
+        ExplodeCell(originCell, middleExplosion);
+
+        bool continueTop = true;
+        bool continueBottom = true;
+        bool continueRight = true;
+        bool continueLeft = true;
+
+        for (int i = 1; i < explosionLength; i++)
+        {
+            if (continueTop)
+                continueTop = ExplodeCell(originCell + new Vector3Int(0, i, 0), verticalExplosion);
+
+            if (continueBottom)
+                continueBottom = ExplodeCell(originCell + new Vector3Int(0, -i, 0), verticalExplosion);
+
+            if (continueLeft)
+                continueLeft = ExplodeCell(originCell + new Vector3Int(-i, 0, 0), horizontalExplosion);
+
+            if (continueRight)
+                continueRight = ExplodeCell(originCell + new Vector3Int(i, 0, 0), horizontalExplosion);
+        }
+        
+
+        if (continueTop)
+            ExplodeCell(originCell + new Vector3Int(0, explosionLength, 0), topEndExplosion);
+
+        if (continueBottom)
+            ExplodeCell(originCell + new Vector3Int(0, -explosionLength, 0), bottomEndExplosion);
+
+        if (continueLeft)
+            ExplodeCell(originCell + new Vector3Int(-explosionLength, 0, 0), leftEndExplosion);
+
+        if (continueRight)
+            ExplodeCell(originCell + new Vector3Int(explosionLength, 0, 0), rightEndExplosion);
+
+    }
+
+    bool ExplodeCell(Vector3Int cell, GameObject animation)
+    {
+        Tile tile = tilemap.GetTile<Tile>(cell);
+
+        if (tile == unbreakableWall)
+        {
+            return false;
+        }
+
+        if (tile == wall)
+        {
+            tilemap.SetTile(cell, null);
+        }
+
+        // Create an explosion
+        Vector3 pos = tilemap.GetCellCenterWorld(cell);
+        Instantiate(animation, pos, Quaternion.identity);
+
+        return true;
+    }
+
+}
